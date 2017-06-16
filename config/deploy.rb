6041bec24f1226e7d3810aff5fe47bd6fe53264b
hungmi <hungmihungmi@gmail.com>
2017-06-16 17:24:06 +0800

    add uploading secrets.yml in job

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,6 +45,7 @@ namespace :deploy do
 	    execute "mkdir #{shared_path}/config -p"
 	    upload! StringIO.new(File.read("config/database.yml")), "#{shared_path}/config/database.yml"
 	    upload! StringIO.new(File.read("config/application.yml")), "#{shared_path}/config/application.yml"
+	    upload! StringIO.new(File.read("config/application.yml")), "#{shared_path}/config/secrets.yml"
 	  end
 	end
 end
