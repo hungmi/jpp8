6ed397dedf4f9818463f0064eed38ab06ff47a6e
hungmi <hungmihungmi@gmail.com>
2017-06-18 20:04:36 +0800

    fix select2 with turbolinks; change turbolinks progress bar color

--- app/assets/javascripts/admin/initialization.js
+++ app/assets/javascripts/admin/initialization.js
@@ -1,4 +1,6 @@
 $(document).on("turbolinks:load", function() {
+	$("select.select2").removeClass("select2-hidden-accessible")
+	$("span.select2-container").remove()
 	$( ".select2" ).select2({
 	  theme: "bootstrap",
 	  language: "zh-TW"
